33e44c343f69f208083dc2b289eb2610f3035514
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2022-11-19 17:45:09 +0300

    v1

---
+++ New Project/Assets/My Game/My Scripts/PlayerScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int coins;
+	public AudioSource mainmusic;
+	public int enemyscount;
+
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start() {
+		coins = 0;
+		enemyscount = 0;
+		mainmusic.Play();
+		
+	}
+
+	// OnTriggerEnter is called when the Collider "collided" enters the trigger.
+	protected void OnTriggerEnter(Collider collided) {
+
+		// Check for collision with coins
+		if (collided.gameObject.tag == "Coin") {  
+			
+			Destroy(collided.gameObject);
+            if (coins == 0)
+            {
+				HUD.Message("You got first coin!");
+            }
+            else
+            {
+				HUD.Message("You got another coin!");
+            }
+
+			coins++;
+			HUD.UpdateCoinCount(coins);
+
+		}
+
+        if (collided.gameObject.name == "Powerup Object 1")
+        {
+			Destroy(collided.gameObject);
+			Abilities.doubleJumpEnabled = true;
+			HUD.Message("Double jump activated!");
+        }
+
+	}
+}
---
+++ New Project/Assets/dangerscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dangerscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int HPs;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnCollisionEnter(Collision col)
+   
+        
+    {
+                // Check for collision with Player
+        if (col.gameObject.tag == "Player")
+        {
+            
+            HPs = HPs - 1;
+            
+
+                    }
+    }
+}
